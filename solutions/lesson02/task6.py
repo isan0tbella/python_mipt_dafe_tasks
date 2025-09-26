541a793c9943b4eb12a1ddd52bbb5db2baab1f7e
Taisia <taisia0106@gmail.com>
2025-09-26 23:30:13 +0300

    Update task6.py

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,4 +1,15 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    # ваш код
+    if num % 2 == 0:
+        sum_of_divisors += 2
+        while num % 2 == 0:
+            num //= 2
+    n = 3
+    while num > 1:
+        if num % n == 0:
+            sum_of_divisors += n
+            while num % n == 0:
+                num //= n
+        n += 2
+
     return sum_of_divisors
