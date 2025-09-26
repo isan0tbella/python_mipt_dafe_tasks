aa474a1dad39f22d592ef167a9a62b5293b6c04e
Taisia <taisia0106@gmail.com>
2025-09-26 23:27:45 +0300

    Update task2.py

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,4 +1,10 @@
 def get_doubled_factorial(num: int) -> int:
     factorial = 1
-    # ваш код
+    if num > 1:
+        for i in range(num, 0, -2):
+            factorial *= i
+
+    else:
+        return 1
+
     return factorial
