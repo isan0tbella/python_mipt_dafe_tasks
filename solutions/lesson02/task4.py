e36cb09b7ac2b5c758e19dd42bdd451438d147e9
Taisia <taisia0106@gmail.com>
2025-09-26 23:29:21 +0300

    Update task4.py

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,12 @@
 def get_multiplications_amount(num: int) -> int:
     multiplications_amount = 0
-    # ваш код
+    if num == 1:
+        return 0
+    while num > 1:
+        if num % 2 == 0:
+            multiplications_amount += 1
+            num = num // 2
+        else:
+            multiplications_amount += 1
+            num = num - 1
     return multiplications_amount
